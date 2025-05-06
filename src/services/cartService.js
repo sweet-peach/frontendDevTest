073a0d48f972fa85d30307b8fd7053b7f95ad7e0
Nikita <127978648+sweet-peach@users.noreply.github.com>
2025-05-06 23:51:09 +0200

    Cart fix

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -3,17 +3,15 @@ import {useCartStore} from "../stores/cartStore.js";
 
 export default class CartService{
     static async addToCart(itemId, colorCode, storageCode){
-
-        console.log(itemId);
-        console.log(colorCode);
-        console.log(storageCode);
         const response = await apiClient.post("api/cart",{
             id: itemId,
             colorCode: colorCode,
             storageCode: storageCode
         })
+        // Does not work as intended
+        // useCartStore.getState().setCartItems(response.data.count);
 
-        useCartStore.getState().setCartItems(response.data.count);
+        useCartStore.getState().addItem();
 
         return response.data;
     }
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,6 +1,11 @@
 import {create} from "zustand";
+import {persist} from "zustand/middleware";
 
-export const useCartStore = create((set) => ({
-    cartItems: 0,
-    setCartItems: (cartItems) => set({cartItems})
-}))
+export const useCartStore = create(persist((set) => ({
+        cartItems: 0,
+        setCartItems: (cartItems) => set({cartItems}),
+        addItem: ()=> set((state)=>{
+            return {cartItems: state.cartItems + 1}
+        })
+    })
+))
